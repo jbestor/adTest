f1b638c6504db9894eb458c5824e5200f444e2dd
Jason Bestor <jbestor@gmail.com>
2015-06-10 10:06:20 -0400

    Initial Commit

---
+++ adTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  adTest
+//
+//  Created by Jason Bestor on 6/10/15.
+//  Copyright (c) 2015 Jason Bestor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ adTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  adTest
+//
+//  Created by Jason Bestor on 6/10/15.
+//  Copyright (c) 2015 Jason Bestor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
